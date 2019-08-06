588e11e0fa0e0b6707fcad9a0e32a1ccfbfac25a
shozawa <tomohiro.shozawa@gmail.com>
2019-08-07 07:46:51 +0900

    calculate pi

---
+++ 01/04/calculation_pi.py
@@ -0,0 +1,14 @@
+def calculate_pi(n_terms: int) -> float:
+    numerator = 4.0
+    denominator = 1.0
+    operation = 1.0
+    pi = 0.0
+    for _ in range(n_terms):
+        pi += operation * (numerator / denominator)
+        denominator += 2.0
+        operation *= -1.0
+    return pi
+
+
+if __name__ == "__main__":
+    print(calculate_pi(1000))
